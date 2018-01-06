a3309a3f754171720d4967bc1e370e8260ea7aa0
c-trl <tirol.christian@gmail.com>
2018-01-06 03:52:26 -0500

    initial commit

---
+++ mpl_settings.py
@@ -0,0 +1,24 @@
+import brewer2mpl
+import matplotlib as mpl
+import pandas as pd
+
+pd.options.display.mpl_style = 'default'
+
+bmap = brewer2mpl.get_map('Set1','qualitative',8,reverse=False)
+colors = bmap.mpl_colors
+
+smallfont = {'family' : 'Open Sans',
+        'weight' : 'normal',
+        'size'   : 8}
+
+font = {'family' : 'Open Sans',
+        'weight' : 'normal',
+        'size'   : 12}
+
+bigfont = {'family' : 'Open Sans',
+        'weight' : 'normal',
+        'size'   : 18,}
+
+mpl.rc('font', **font)
+mpl.rc('lines', linewidth=1)
+mpl.rcParams['legend.fontsize'] = 10
